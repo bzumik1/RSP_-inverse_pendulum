f2ccc86501e6c716e04f5612e55cbfed3244f108
000 <krystof.bystricky@fs.cvut.cz>
2020-02-28 18:22:39 +0100

    Diskretni full-state regulator + jeho testovani

--- Courseware Resources/Software/Linear Inverted Pendulum/SIP_ABCD_eqns.m
+++ Courseware Resources/Software/Linear Inverted Pendulum/SIP_ABCD_eqns.m
@@ -14,6 +14,6 @@ B = [0; 0; (Jp+Mp*lp^2)/Jt; Mp*lp/Jt];
 C = eye(2,4);
 D = zeros(2,1);
 
-A(3,3) = A(3,3) - B(3)*eta_g*Kg^2*eta_m*Kt*Km/r_mp^2/Rm;
-A(4,3) = A(4,3) - B(4)*eta_g*Kg^2*eta_m*Kt*Km/r_mp^2/Rm;
-B = eta_g*Kg*eta_m*Kt/r_mp/Rm*B;
+%A(3,3) = A(3,3) - B(3)*eta_g*Kg^2*eta_m*Kt*Km/r_mp^2/Rm;
+%A(4,3) = A(4,3) - B(4)*eta_g*Kg^2*eta_m*Kt*Km/r_mp^2/Rm;
+%B = eta_g*Kg*eta_m*Kt/r_mp/Rm*B;A2
--- animation.m
+++ animation.m
@@ -1,10 +1,14 @@
 close all
 
-data = solutionNonlinear.y;
-Time = solutionNonlinear.x;
+%data = solutionNonlinearLQR.y;
+%Time = solutionNonlinearLQR.x; 
+
+data = solutionNonlinearPP.y;
+Time = solutionNonlinearPP.x; 
+
 
 timeSeries = timeseries(data, Time);
-dt = 0.1;
+dt = 0.05;
 timeSeries = timeSeries.resample(timeSeries.Time(1):dt:timeSeries.Time(end));
 
 rows_ = size(timeSeries.Time);
@@ -15,10 +19,12 @@ f1 = figure;
 % animaci lze zrychlit pomocí úpravy kroku, nap?. p?i 1:3:rows_ se
 % vykresluje pouze každý t?etí vzorek
 for row = 1:1:rows_    
-    alpha = timeSeries.Data(1, 1, row);
-    Dalpha = timeSeries.Data(2, 1, row);
-    xc = timeSeries.Data(3, 1, row);
-    Dxc = timeSeries.Data(4, 1, row);
+
+    alpha = timeSeries.Data(2, 1, row);
+    Dalpha = timeSeries.Data(4, 1, row);
+
+    xc = timeSeries.Data(1, 1, row);
+    Dxc = timeSeries.Data(3, 1, row);
    
     %poloha kyvadla
     [xp, yp] = pol2cart(alpha-pi/2, L_p);
@@ -28,14 +34,15 @@ for row = 1:1:rows_
     grid on
     
     axis equal
-    xlim([xc+-1-L_p*1.5, xc+1+L_p*1.5])
-    ylim([-L_p*1.5, L_p*1.5])
+    %xlim([xc*1-0.1-L_p*0.5, xc*1+0.1+L_p*0.5])
+    xlim([min(timeSeries.Data(1,1,:))-0.5, max(timeSeries.Data(1,1,:)+0.5)]);
+    ylim([-L_p*1.5, L_p*1.5]);
     
     quiver( xc, 0,...
         xp, yp,...
         'Color', 'Black',...
         'LineWidth', 2,...
-        'Marker', '*')
+        'Marker', '*');
 
     time = timeSeries.Time(row);
     title("T = " + time + "   x = " + xc);
